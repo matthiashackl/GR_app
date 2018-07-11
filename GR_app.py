5a6d953b770520581ac206d677a9072905d7032e
hackl <matthias_hackl@gmx.de>
2018-07-11 20:19:19 +0200

    add readme

--- GR_app.py
+++ GR_app.py
@@ -4,7 +4,7 @@
 # basic information about seismicity in an area of interest.
 #
 # This is a web server application with an interactive web map that makes use of scientific data in a geospatial
-# context. It applies a set of libraries from the scientific Python stack, to estimate the magnitude of completeness
+# context. It applies a set of libraries from the scientific Python stack to estimate the magnitude of completeness
 # and fit the Gutenberg Richter relationship to the magnitude frequency distribution of the earthquakes in an user
 # selected area. The user interface consists of an interactive map, plot, and text information.
 #
